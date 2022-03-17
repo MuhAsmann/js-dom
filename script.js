465584f14a7711b5a86855b01adb84f734d96596
Muhammad Asman <muhasmangit@gmail.com>
2022-03-17 14:23:20 +0700

    projek sederhana bermain dengan warna

--- script.js
+++ script.js
@@ -1,71 +1,9 @@
-console.info("DOM EVENTS");
+const tombol = document.getElementById("tombol");
 
-/*
-    EVENTS != EVEN
-    1. event pada javascript merepresentasikan sebuah kejadian yang terjadi didalam DOM
-    2. kejadian tersebut bisa dilakukan user contoh(mouse event,keyboard event dll)
-    3. ataupun bisa dilakukan secara otomatis oleh api(animasi selesai dijalankan, halaman selesai di load, dll)
+// const body = document.body;
 
-    CARA 'MENDENGARKAN' (menjalankan) EVENT
-    1. EVENT HANDLER
-        - INLINE HTML ATTRIBUTE (lansung simpan di html line)
-        - ELEMENT METHOD
-    2. addEventListener()
-
-    CATATAN PENTING
-    1. JIKA MENGGUNAKAN EVENT HANDLER, JIKA MEMBUAT EVENT LAGI DIBAWAHNYA MAKA EVENT SEBELUMNYA TIDAK AKAN DIEKSEKUSI
-    2. JIKA MENGGUNAKAN ADDEVENT BERBEDA DENGAN EVENT HANDLER, JIKA MENGGUNAKAN ADDEVENT FUNGSI ATAU EVENT SEBELUMNYA TETAP BISA TERJALANKAN
-
-    MOUSE EVENT
-    1. CLICK
-    2. DBLCLICK
-    3. MOUSEOVER
-    4. MOUSEENTER
-    5. MOUSEUP
-    6. WHEEL
-    7. MOUSELEAVE
-    DLL
-
-    KEYBOARD EVENT
-    1. KEYDOWN
-    2. KEYPRESS
-    3. KEYUP
-
-    VIEW EVENT
-    1. RESIZE
-    2. SCROLL
-
-    FORM EVENT
-    1. RESET
-    2. SUBMIT
-*/
-
- //event handler
-
-    //inline html
-
-     const p3 = document.querySelector(".p3");
-
-     //kita panggil dan menyimpan function kedalam element di html
-     function ubahWarna(value) {
-         p3.style.backgroundColor = "blue";
-     }
-     function ubahWarna2(value) {
-         p2.style.backgroundColor = "blue";
-     }
-
-    //element method
-
-     const p2 = document.querySelector(".p2");
-     p2.onclick = ubahWarna2;
-
-//addEventListener
-     const p4 = document.querySelector("section#b p");
-     p4.addEventListener("click", function() {
-         const ul = document.querySelector("section#b ul");
-         const li = document.createElement("li");
-         const textLi = document.createTextNode("Item Baru");
-         
-         li.appendChild(textLi);
-         ul.appendChild(li);
-     });
+tombol.onclick = function () {
+    // document.body.style.backgroundColor = "lightblue";
+    // document.body.setAttribute("class","warna-biru");
+    document.body.classList.toggle("warna-biru");
+}
