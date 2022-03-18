81a96238239a619c004f9c832577d532868ac4e7
Muhammad Asman <muhasmangit@gmail.com>
2022-03-18 09:15:48 +0700

    Bermain Dengan Warna lanjut

--- script.js
+++ script.js
@@ -7,3 +7,45 @@ tombol.onclick = function () {
     // document.body.setAttribute("class","warna-biru");
     document.body.classList.toggle("warna-biru");
 }
+
+const tAcakWarna = document.createElement("button");
+const teksTombol = document.createTextNode("acak warna");
+tAcakWarna.appendChild(teksTombol);
+tAcakWarna.setAttribute("type", "button");
+tombol.after(tAcakWarna);
+
+tAcakWarna.addEventListener("click", function () {
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = "rgb("+ r +","+ g +","+ b +")";
+});
+
+const sMerah = document.querySelector("input[name=sMerah]");
+const sBiru = document.querySelector("input[name=sBiru]");
+const sHijau = document.querySelector("input[name=sHijau]");
+
+sMerah.addEventListener("input", function () {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = "rgb("+ r +","+ g +","+ b +")";
+});
+sHijau.addEventListener("input", function () {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = "rgb("+ r +","+ g +","+ b +")";
+});
+sBiru.addEventListener("input", function () {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = "rgb("+ r +","+ g +","+ b +")";
+});
+
+document.body.addEventListener("mousemove", function (event) {
+    const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+    const YPos = Math.round((event.clientY / window.innerHeight) * 255);
+    document.body.style.backgroundColor = "rgb("+ xPos +","+ YPos +",100)";
+})
